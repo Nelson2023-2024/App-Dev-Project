094c0ac716d0fd04479021178117bb5e237ae0e3
unknown <nelsonobuya19@gmail.com>
2024-01-27 17:49:11 +0300

    Added the PHP for displaying the current details and creating the form and styling with bootstrap for the edit.php

---
+++ edit.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <title>Edit</title>
+
+    <style>
+    form {
+        max-width: 600px;
+    }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <a style="font-size: 2rem;" href="./admin-panel.php"><i class="bi bi-house"></i>
+            home
+        </a>
+
+        <h3 class="text-center">Current details</h3>
+        <table class="table table-striped mt-2">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Gender</th>
+                    <th>User Level</th>
+                    <th>Registration Date</th>
+                </tr>
+            </thead>
+
+            <?php
+try{
+    require("./mysqli_connect.php");
+
+    if($_SERVER['REQUEST_METHOD'] === "GET"){
+    
+        $id = $_GET['id'];
+
+        $select_user_stmt = mysqli_stmt_init($dbcon);
+        $select_user_query = "SELECT * FROM users WHERE id = ?";
+        mysqli_stmt_prepare($select_user_stmt, $select_user_query);
+        mysqli_stmt_bind_param($select_user_stmt, 'i', $id );
+        mysqli_stmt_execute($select_user_stmt);
+
+        $result =  mysqli_stmt_get_result($select_user_stmt);
+
+        if($result){
+            $row = mysqli_fetch_assoc($result);
+
+            echo "
+            <tr>
+            <td>$row[id]</td>
+            <td>$row[first_name]</td>
+            <td>$row[last_name]</td>
+            <td>$row[email]</td>
+            <td>$row[phone_number]</td>
+            <td>$row[gender]</td>
+            <td><strong>".($row['user_level'] === 0 ? "USER" : "ADMIN")."</strong></td>
+            <td>$row[registration_date]</td>
+            
+            
+            </tr>
+            ";
+
+            echo"</table>
+            ";
+
+        }
+
+        
+    
+    }
+    //POST METHOD
+    if($_SERVER['REQUEST_METHOD'] === "POST"){
+        $first_name = trim($_POST['first_name']);
+        $last_name = trim($_POST['last_name']);
+        $email = trim($_POST['email']);
+        $phone_number = trim($_POST['phone_number']);
+        $gender = trim($_POST['gender']);
+        $user_level = trim($_POST['user_level']);
+
+        $update_user_stmt = mysqli_stmt_init($dbcon);
+    }
+}
+
+//database exception
+catch (mysqli_sql_exception $e){
+    echo "Database Exception".$e->getMessage();
+
+}
+//general exception
+catch (Exception $e){
+    echo "General Exception".$e->getMessage();
+}
+?>
+
+            <h1 class="text-center mt-3">Update </h1>
+            <form action="" method="post" class="mx-auto">
+
+
+                <!-- names -->
+                <div class="row mb-2">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="First name" name="first_name">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Last name" name="last_name">
+                    </div>
+                </div>
+
+                <!-- Email -->
+                <div class="row mb-2">
+                <div class="col">
+                        <input type="text" class="form-control" placeholder="Email" name="email">
+                    </div>
+                </div>
+
+                <!-- phone number -->
+                <div class="row mb-2">
+                <div class="col">
+                        <input type="text" class="form-control" placeholder="Phone Number" name="phone_number">
+                    </div>
+                </div>
+
+                <!-- gender -->
+                <div class="row mb-3">
+                <div class="col">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="gender" id="maleRadio" value="male">
+                        <label class="form-check-label" for="maleRadio">
+                            Male
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="gender" id="femaleRadio" value="female">
+                        <label class="form-check-label" for="femaleRadio">
+                            Female
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="gender" id="otherRadio" value="other">
+                        <label class="form-check-label" for="otherRadio">
+                            others
+                        </label>
+                    </div>
+                </div>
+                <span id="gender_error"></span>
+            </div>
+
+            <!-- phone number -->
+            <div class="row mb-2">
+                <div class="col">
+                        <input type="number" class="form-control" placeholder="User level" name="user_level">
+                    </div>
+            </div>
+
+            <div class="row mb-2">
+                <div class="col">
+                <button style="width: 100%;" type="sumbit" class="btn btn-success">update</button>
+                </div>
+            </div>
+
+
+            </form>
+    </div>
+
+</body>
+
+</html>
