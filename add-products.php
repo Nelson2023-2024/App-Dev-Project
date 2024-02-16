f7db73c5cb04898c5b1e5f119fe46704096af0d7
unknown <nelsonobuya19@gmail.com>
2024-02-16 14:19:59 +0300

    Added the HTML and styling with bootstrap on the Add-products.php

--- add-products.php
+++ add-products.php
@@ -12,18 +12,50 @@
 
     <!-- titan -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <style>
+        body{
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
     <title>Document</title>
 </head>
 
 <body>
     <div class="container">
-        <form action="">
-            <div class="input-group">
-                <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
-                <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Button</button>
+        <form action="" class="mx-auto border  py-5 px-2" style="width: 600px;">
+            <h1 class="text-center">Add New Product FrontEnd</h1>
+
+            <div class="row g-3 mb-3">
+                <div class="">
+                    <input type="text" class="form-control" placeholder="Title" aria-label="First name">
+                </div>
             </div>
+
+            <div class="input-group mb-3">
+                <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+            </div>
+
+            <div class="row g-3 mb-3">
+                <div class="">
+                    <input type="text" class="form-control" placeholder="Price" aria-label="First name">
+                </div>
+            </div>
+
+            <div class=" mb-3">
+                <textarea style="width: 100%;" class="form-control" name="" id="" cols="10" rows="5" placeholder=" Description"></textarea>
+            </div>
+
+            <button style="width: 100%; height: 40px;" class="btn btn-primary">Add New Product</button>
+
         </form>
     </div>
+
+
+    <script src=""></script>
 </body>
 
 </html>
