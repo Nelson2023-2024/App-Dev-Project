66545cf3296d63fa69bb97d26b0a757155f8577e
NelsonSPU <nelsonobuya19@gmail.com>
2024-03-15 11:02:38 +0300

    Changed the href links to their appropriate links on the navbar

--- navbar.php
+++ navbar.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,23 +13,25 @@
 
     <!-- titan -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Titan+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titan+One&display=swap" rel="stylesheet">
     <title>Document</title>
     <style>
-         nav{
+        nav {
             display: flex;
             justify-content: space-between;
             align-items: center;
             background-color: royalblue;
             height: 70px;
         }
-        nav ul{
+
+        nav ul {
             display: flex;
             gap: 2rem;
             list-style: none;
         }
-        nav .navbar-brand{
+
+        nav .navbar-brand {
             font-family: "Titan One", sans-serif;
             font-style: italic;
             font-size: 2rem;
@@ -37,38 +40,44 @@
             background-clip: text;
             padding-inline: 2rem;
         }
-        nav ul{
+
+        nav ul {
             padding-right: 1rem;
         }
-        nav ul a{
+
+        nav ul a {
             color: #fff;
             text-decoration: none;
             transition: .3s ease-in-out;
         }
-        nav ul a:hover{
-            color:orangered;
-            
+
+        nav ul a:hover {
+            color: orangered;
+
         }
-        ul li a{
+
+        ul li a {
             display: flex;
             align-items: center;
             gap: 10px;
         }
     </style>
 </head>
+
 <body>
-<nav>
+    <nav>
         <!-- navbrnad -->
-        <a class="navbar-brand" href="./admin-panel.php">Adminpanel</a>
+        <a class="navbar-brand" href="./dashboard.php">Adminpanel</a>
 
         <!-- navlinks -->
         <ul>
-            <li><a href="./admin-panel.php"><i class="bi bi-pen-fill"></i> Manage users</a></li>
+            <li><a href="./manageusers.php"><i class="bi bi-pen-fill"></i> Manage users</a></li>
             <li><a href="./search.php"><i class="bi bi-search"></i> Seach users</a></li>
             <li><a href="./registration-admin.php"><i class="bi bi-plus-circle"></i>Add New users</a></li>
             <li><a href="./Add-products.php"><i class="bi bi-plus-circle"></i>Add Products</a></li>
             <li><a href="./logout.php"><i class="bi bi-box-arrow-left"></i>Log Out</a></li>
         </ul>
-        </nav>
+    </nav>
 </body>
+
 </html>
