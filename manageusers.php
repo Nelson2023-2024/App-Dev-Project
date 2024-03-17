4161f651fa46f53eab0cf62a4300a530699e7bbc
NelsonSPU <nelsonobuya19@gmail.com>
2024-03-17 16:32:19 +0300

    Added icon link on manageuser.php

--- manageusers.php
+++ manageusers.php
@@ -30,6 +30,8 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./bootstrap-5.0.2-dist/css/bootstrap.min.css">
+      <!-- bootstrap icons -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <title>Admin-panel</title>
 
